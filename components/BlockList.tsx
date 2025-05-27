7a57c5c97ec26ec0351b1b7f9a036935b2a68ef4
Andrey Balabanov <balabaniada06@mail.ru>
2025-05-27 15:26:25 +0700

    Added select block component, reworked BlockList

--- components/BlockList.tsx
+++ components/BlockList.tsx
@@ -21,6 +21,7 @@ import { uuidv4 } from "../shared/functions";
 import CCodeBlockWhile from "../classes/CodeBlockWhile";
 import Renderable from "../shared/Interfaces/Renderable";
 import CCodeBlockBreak from "../classes/CodeBlockBreak";
+import SelectBlock from "./SelectBlock";
 
 interface Props {
     isVisible: Boolean;
@@ -34,33 +35,7 @@ interface Props {
 }
 
 const BlockList = (props: Props) => {
-    const newBlocks: Renderable[] = [
-        new CCodeBlockAsigment(
-            { x: 0, y: 0 },
-            new CCodeBlockWrapper({ x: 0, y: 0 }, null),
-            props.onDrop
-        ),
-        new CCodeBlockPrint(
-            { x: 0, y: 0 },
-            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
-            props.onDrop,
-            props.globalOutput,
-            props.globalSetOutput
-        ),
-        new CCodeBlockValue({ x: 0, y: 0 }, props.onDrop),
-        new CCodeBlockGetVariableValue({ x: 0, y: 0 }, props.onDrop),
-        new CCodeBlockLogic(
-            { x: 0, y: 0 },
-            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
-            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
-            props.onDrop
-        ),
-        new CCodeBlockMath(
-            { x: 0, y: 0 },
-            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
-            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
-            props.onDrop
-        ),
+    const flowControl: Renderable[] = [
         new CCodeBlockIfStatement(
             { x: 0, y: 0 },
             new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
@@ -76,6 +51,39 @@ const BlockList = (props: Props) => {
         ),
         new CCodeBlockBreak({ x: 0, y: 0 }, props.onDrop),
     ];
+    const utility = [
+        new CCodeBlockAsigment(
+            { x: 0, y: 0 },
+            new CCodeBlockWrapper({ x: 0, y: 0 }, null),
+            props.onDrop
+        ),
+        new CCodeBlockPrint(
+            { x: 0, y: 0 },
+            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
+            props.onDrop,
+            props.globalOutput,
+            props.globalSetOutput
+        ),
+        new CCodeBlockValue({ x: 0, y: 0 }, props.onDrop),
+        new CCodeBlockGetVariableValue({ x: 0, y: 0 }, props.onDrop),
+    ];
+    const operators = [
+        new CCodeBlockLogic(
+            { x: 0, y: 0 },
+            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
+            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
+            props.onDrop
+        ),
+        new CCodeBlockMath(
+            { x: 0, y: 0 },
+            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
+            new CCodeBlockWrapper({ x: 0, y: 0 }, null, null),
+            props.onDrop
+        ),
+    ];
+    const [isUtilityOpen, setIsUtilityOpen] = useState(false);
+    const [isFlowControlOpen, setIsFlowControlOpen] = useState(false);
+    const [isOperatorsOpen, setIsOperatorsOpen] = useState(false);
     const [, renderer] = useReducer((e) => e - 1, 0);
     return (
         <ScrollView
@@ -83,9 +91,27 @@ const BlockList = (props: Props) => {
                 display: props.isVisible ? "flex" : "none",
                 ...styles.container,
             }}>
-            {newBlocks.map((item) =>
-                item.render({ key: uuidv4(), renderer: renderer })
-            )}
+            <SelectBlock
+                blocks={utility}
+                text="Утилитарные"
+                isOpen={isUtilityOpen}
+                setIsOpen={setIsUtilityOpen}
+                rerender={renderer}
+            />
+            <SelectBlock
+                blocks={flowControl}
+                text="Контроль потока"
+                isOpen={isFlowControlOpen}
+                setIsOpen={setIsFlowControlOpen}
+                rerender={renderer}
+            />
+            <SelectBlock
+                blocks={operators}
+                text="Операторы"
+                isOpen={isOperatorsOpen}
+                setIsOpen={setIsOperatorsOpen}
+                rerender={renderer}
+            />
         </ScrollView>
     );
 };
---
+++ components/SelectBlock.tsx
@@ -0,0 +1,51 @@
+import {
+    Animated,
+    GestureResponderEvent,
+    PanResponderGestureState,
+    Pressable,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
+import Draggable from "./Draggable";
+import { uuidv4 } from "../shared/functions";
+import { PropsWithChildren, Dispatch, DispatchWithoutAction } from "react";
+import Renderable from "../shared/Interfaces/Renderable";
+import { globalStyles } from "../shared/globalStyles";
+
+interface Props {
+    isOpen: boolean;
+    setIsOpen: Dispatch<boolean>;
+    blocks: Renderable[];
+    rerender: DispatchWithoutAction;
+    text: string;
+}
+
+const SelectBlock = (props: Props & PropsWithChildren) => {
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={() => props.setIsOpen(!props.isOpen)}>
+                <Text style={styles.text}>{`▾${props.text}`}</Text>
+            </Pressable>
+            {props.isOpen &&
+                props.blocks.map((item) =>
+                    item.render({
+                        rerender: props.rerender,
+                        key: uuidv4(),
+                    })
+                )}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: globalStyles.fontSize,
+        fontFamily: globalStyles.fontFamily,
+        color: globalStyles.backgroundColor,
+    },
+    container: {},
+});
+
+export default SelectBlock;
+
